241f540365f1d3bc9d3eb48a8f7cb8b35697edc6
Katherine Negrete <katherine.negrete.aguilar@gmail.com>
2022-09-29 19:43:33 -0500

    content(code): add javascript code

--- big-companies-gallery/main.js
+++ big-companies-gallery/main.js
@@ -1,7 +1,17 @@
 const title = document.querySelector("#company-title");
-const disneyImage = document.querySelector(".image-disney");
+const images = document.querySelectorAll("img");
+const imagesArray = [...images];
+console.log(imagesArray);
+
+const widthAndHeight = imagesArray.map((image) => {
+  image.style.width = "450px";
+  image.style.height = "380px";
+  image.style.margin = "50px";
+});
+
 // styles
 title.style.textAlign = "center";
+
 // functions
 const titleMessage = (e) => {
   console.log(`hello`);
